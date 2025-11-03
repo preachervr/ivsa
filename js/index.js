cdf6997b41e5bbcf0ca9ec3324c57123c7986099
preachervr <preachervr@gmail.com>
2025-11-03 19:10:36 +0200

    Updated javascritp for index

--- js/index.js
+++ js/index.js
@@ -3,9 +3,8 @@
 const links = document.querySelectorAll('#navLinks a');
 links.forEach(link => {
   if (link.href === window.location.href) {
-    // Add the same underline effect that happens on hover
-    link.classList.add('before:w-full', 'text-blue-400');
+    link.classList.add('before:w-full', 'text-blue-300');
   } else {
-    link.classList.remove('before:w-full', 'text-blue-400');
+    link.classList.remove('before:w-full', 'text-blue-300');
   }
 });
